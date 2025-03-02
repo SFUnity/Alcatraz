268bd86000556ac4d59e3279cf2747325fe18473
S C <bramblestar1.0@icloud.com>
2025-03-02 13:17:28 -0800

    started climb

---
+++ src/main/java/frc/robot/subsystems/climb/ClimbIO.java
@@ -0,0 +1,12 @@
+package frc.robot.subsystems.climb;
+
+import org.littletonrobotics.junction.AutoLog;
+
+public interface ClimbIO {
+    @AutoLog
+    public static class ClimbIOInputs {
+    
+        
+    }
+    
+}
