5e4a8c2f34229334c7ec9c970157b8e7af0f5409
Mishi Doron <mishi.doron@gmail.com>
2025-11-01 16:23:25 -0700

    Add delay for carrige eject for real

--- src/main/java/frc/robot/Autos.java
+++ src/main/java/frc/robot/Autos.java
@@ -230,7 +230,7 @@ public class Autos {
 
     cToCD_Algae.active().onTrue(dealgify(elevator, carriage, poseManager, () -> cToCD_Algae.getFinalPose().get(), cToCD_Algae.active().negate()));
 
-    cToCD_Algae.done().onTrue(waitUntil(() -> !carriage.algaeHeld()).andThen(cD_AlgaeToFeeding.cmd().asProxy()));
+    cD_AlgaeToFeeding.active().onTrue(waitSeconds(1).andThen(carriage.ejectAlgae()));
 
     cD_AlgaeToFeeding.active().onTrue(carriage.ejectAlgae());
 
