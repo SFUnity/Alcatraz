f72b708964a596a6ee51d81657b7b04eaa4bd843
MrTinker64 <avi.micah@gmail.com>
2026-01-07 10:24:37 -0800

    format

--- src/main/java/frc/robot/subsystems/funnel/Funnel.java
+++ src/main/java/frc/robot/subsystems/funnel/Funnel.java
@@ -6,7 +6,6 @@ import java.util.function.BooleanSupplier;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.subsystems.carriage.Carriage;
 import frc.robot.subsystems.carriage.CarriageConstants;
 import org.littletonrobotics.junction.Logger;
 
